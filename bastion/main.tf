b4a395e627cd6bb7a0cd13e47d4ed1e7f3ae8bce
misc <michael.schimmer@loadbee.com>
2018-02-20 10:05:28 +0100

    added ttl

--- bastion/main.tf
+++ bastion/main.tf
@@ -93,6 +93,7 @@ resource "aws_route53_record" "main" {
   zone_id = "${var.zone_id}"
   name    = "${var.dns_name}"
   type    = "CNAME"
+  ttl     = 300
   records = ["${aws_eip.bastion.public_ip}"]
 }
 
